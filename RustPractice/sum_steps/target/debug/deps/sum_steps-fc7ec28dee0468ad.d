d2a2b835647045720c5a62e0e39530ca9d95de83
Rebecca Fernandez <97413846+beckyfernez@users.noreply.github.com>
2024-10-10 01:47:50 +0000

    sum w steps added

---
+++ RustPractice/sum_steps/target/debug/deps/sum_steps-fc7ec28dee0468ad.d
@@ -0,0 +1,5 @@
+/workspaces/osfall2024/RustPractice/sum_steps/target/debug/deps/sum_steps-fc7ec28dee0468ad: src/main.rs
+
+/workspaces/osfall2024/RustPractice/sum_steps/target/debug/deps/sum_steps-fc7ec28dee0468ad.d: src/main.rs
+
+src/main.rs:
---
+++ RustPractice/sum_steps/target/debug/sum_steps.d
@@ -0,0 +1,1 @@
+/workspaces/osfall2024/RustPractice/sum_steps/target/debug/sum_steps: /workspaces/osfall2024/RustPractice/sum_steps/src/main.rs
